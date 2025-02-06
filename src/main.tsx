56e927b14c601f6afa14d24ca2da4d8b34818d17
Pavel <pavelmalyv@gmail.com>
2025-02-06 20:46:27 +0300

    feat: добавить базовые стили
    Довить библиотеки: material-symbols, @fontsource/inter

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 
+import 'material-symbols';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
+import './styles/base/index.scss';
+
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<App />
