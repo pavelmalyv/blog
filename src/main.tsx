ddff98011af9c6095375746248f4220b8b5607da
Pavel <pavelmalyv@gmail.com>
2025-02-07 14:55:34 +0300

    feat: настройка RTKQ

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './store/store.ts';
 import App from './App.tsx';
 import Modal from 'react-modal';
 
@@ -14,6 +16,8 @@ Modal.setAppElement('#root');
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</StrictMode>,
 );
