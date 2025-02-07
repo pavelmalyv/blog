6925ec36dee6fa08e6497c5fb085b273450168a7
Pavel <pavelmalyv@gmail.com>
2025-02-07 15:02:22 +0300

    feat: добавить вывод постов в RecentPosts

--- src/components/recentPosts/RecentPosts.tsx
+++ src/components/recentPosts/RecentPosts.tsx
@@ -1,11 +1,27 @@
+import type { Posts } from '../../types/posts';
+import cl from './RecentPosts.module.scss';
 import { useGetPostsQuery } from '../../api/postsSlice';
 import Section from '../UI/Section/Section';
 
 const RecentPosts = () => {
-	const { data } = useGetPostsQuery();
-	console.log(data);
+	let posts: Posts | null[] = Array(6).fill(null);
 
-	return <Section title="Recent blog posts">Section</Section>;
+	const { data, isLoading } = useGetPostsQuery();
+	if (!isLoading && data) {
+		posts = data.posts;
+	}
+
+	return (
+		<Section title="Recent blog posts">
+			<ul className={cl.list}>
+				{posts.map((post, i) => {
+					const key = post ? post.id : i;
+
+					return <li key={key}>{post?.title}</li>;
+				})}
+			</ul>
+		</Section>
+	);
 };
 
 export default RecentPosts;
