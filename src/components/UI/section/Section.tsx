fa3e35365ad06dff80780a3d7a23f2fed879cf4f
Pavel <pavelmalyv@gmail.com>
2025-02-07 17:51:14 +0300

    feat: добавить aria-labelledby компоненту Section

--- src/components/UI/section/Section.tsx
+++ src/components/UI/section/Section.tsx
@@ -1,16 +1,23 @@
+import { useId } from 'react';
 import cl from './Section.module.scss';
 
 interface SectionProps {
 	title: string;
 	children: React.ReactNode;
+	region?: boolean;
 }
 
-const Section = ({ title, children }: SectionProps) => {
+const Section = ({ title, children, region = true }: SectionProps) => {
+	const idTitle = useId();
+	const ariaLabelledby = region ? idTitle : undefined;
+
 	return (
-		<section className="section">
+		<section className="section" aria-labelledby={ariaLabelledby}>
 			<div className="container">
 				<div className={cl.head}>
-					<h2 className="h2">{title}</h2>
+					<h2 id={idTitle} className="h2">
+						{title}
+					</h2>
 				</div>
 
 				{children}
