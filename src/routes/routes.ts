df31e66a767eed561469255add422399596bce02
Pavel <pavelmalyv@gmail.com>
2025-02-12 12:36:45 +0300

    refactor(routes): убрать "/" в конце маршрутов

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,6 +2,6 @@ import type { BlogUrl } from '../types/routes';
 
 export const blogUrl: BlogUrl = {
 	base: '/',
-	post: (id) => `/post/${id}/`,
-	pagination: (page) => `/page/${page}/`,
+	post: (id) => `/post/${id}`,
+	pagination: (page) => `/page/${page}`,
 };
