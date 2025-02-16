422336017ca54cab3eea58e9a904838fc99338de
Pavel <pavelmalyv@gmail.com>
2025-02-16 14:33:17 +0300

    feat(date): добавить вывод дня недели

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,7 +1,26 @@
-export const getDisplayDate = (date: string): string => {
-	return new Date(date).toLocaleDateString('en-GB', {
+interface getDisplayDateOptions {
+	weekday?: boolean;
+}
+
+export const getDisplayDate: (date: string, options?: getDisplayDateOptions) => string = (
+	date,
+	options = {},
+) => {
+	const dateConstructor = new Date(date);
+
+	const displayDate = dateConstructor.toLocaleDateString('en-GB', {
 		day: 'numeric',
 		month: 'short',
 		year: 'numeric',
 	});
+
+	if (options.weekday) {
+		const weekday = dateConstructor.toLocaleDateString('en-GB', {
+			weekday: options.weekday ? 'long' : undefined,
+		});
+
+		return `${weekday}, ${displayDate}`;
+	}
+
+	return displayDate;
 };
