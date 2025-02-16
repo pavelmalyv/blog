819d1d41ebbd542ce1f0fab75575939a5b4bef15
Pavel <pavelmalyv@gmail.com>
2025-02-16 13:24:27 +0300

    feat(Article): добавить минимальный вывод контента

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -1,21 +1,50 @@
-import { useId } from 'react';
 import type { Post } from '../../types/posts';
 
+import classNames from 'classnames';
 import cl from './Article.module.scss';
 import Skeleton from 'react-loading-skeleton';
+import Tags from '../UI/tags/Tags';
+import parse from 'html-react-parser';
+
+import { Link } from 'react-router';
+import { useId } from 'react';
 
 interface ArticleProps {
 	post: Post | null;
+	labelledby?: boolean;
 }
 
-const Article = ({ post }: ArticleProps) => {
+const Article = ({ post, labelledby = true }: ArticleProps) => {
 	const idArticle = useId();
 
 	return (
-		<article className={cl.article} aria-labelledby={idArticle}>
-			<h1 id="idArticle" className="h1">
+		<article className={cl.article} aria-labelledby={labelledby ? idArticle : undefined}>
+			<div className={classNames('date', cl.about)}>
+				<div className={cl.date}>Sunday , 1 Jan 2023</div>
+				<div className={cl.author}>
+					<div className={cl['author-label']}>Author:</div>
+					<Link to={'other'} className={cl['author-link']}>
+						Aria Parker
+					</Link>
+				</div>
+			</div>
+			<h1 id={idArticle} className={classNames('h1', cl.title)}>
 				{post ? post.title : <Skeleton />}
 			</h1>
+			{post && (
+				<picture className={cl['image']}>
+					<source srcSet={post.image.webp} type="image/webp" />
+					<img
+						className={cl['image-img']}
+						src={post.image.src}
+						width={post.image.width}
+						height={post.image.height}
+						alt={post.image.alt}
+					/>
+				</picture>
+			)}
+			<div className={classNames('text', cl.body)}>{post && parse(post.body)}</div>
+			<div className={cl.tags}>{post && <Tags tags={post.tags} />}</div>
 		</article>
 	);
 };
