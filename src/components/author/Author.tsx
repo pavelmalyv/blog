1d4cd57d8e91edcb31078fd62e93ee8a7a762441
Pavel <pavelmalyv@gmail.com>
2025-02-16 15:16:20 +0300

    feat(Author): добавить ссылку на страницу автора

--- src/components/author/Author.tsx
+++ src/components/author/Author.tsx
@@ -1,9 +1,12 @@
+import cl from './Author.module.scss';
 import HiddenLoadingMessage from '../UI/hiddenLoadingMessage/HiddenLoadingMessage';
 import Skeleton from 'react-loading-skeleton';
 import ErrorMessage from '../UI/errorMessage/ErrorMessage';
 
 import { useEffect } from 'react';
+import { Link } from 'react-router';
 import { useLazyGetUserByIdQuery } from '../../api/usersSlice';
+import { authorUrl } from '../../routes/routes';
 import { ERROR_MESSAGES } from '../../constants/error';
 import { MESSAGES } from '../../constants/messages';
 
@@ -28,7 +31,11 @@ const Author = ({ id }: AuthorProps) => {
 	if (isError) {
 		author = <ErrorMessage message={ERROR_MESSAGES.userLoad} />;
 	} else if (data && id) {
-		author = data.firstName + ' ' + data.lastName;
+		author = (
+			<Link to={authorUrl.profile(data.id)} className={cl.author}>
+				{data.firstName + ' ' + data.lastName}
+			</Link>
+		);
 	} else {
 		author = <Skeleton width="4em" />;
 	}
