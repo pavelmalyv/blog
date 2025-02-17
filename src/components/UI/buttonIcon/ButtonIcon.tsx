7afbc24e7b2e59ef5d3a2fda41753f5d03be8277
Pavel <pavelmalyv@gmail.com>
2025-02-17 10:48:11 +0300

    fix(ButtonIcon): добавить type

--- src/components/UI/buttonIcon/ButtonIcon.tsx
+++ src/components/UI/buttonIcon/ButtonIcon.tsx
@@ -2,14 +2,15 @@ import Icon from '../icon/Icon';
 import cl from './ButtonIcon.module.scss';
 
 interface ButtonIconProps {
+	type?: 'button' | 'submit' | 'reset';
 	hiddenName: string;
 	icon: string;
 	onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-const ButtonIcon = ({ hiddenName, icon, onClick }: ButtonIconProps) => {
+const ButtonIcon = ({ type = 'button', hiddenName, icon, onClick }: ButtonIconProps) => {
 	return (
-		<button className={cl.button} onClick={onClick}>
+		<button type={type} className={cl.button} onClick={onClick}>
 			<span className="visually-hidden">{hiddenName}</span>
 			<Icon>{icon}</Icon>
 		</button>
