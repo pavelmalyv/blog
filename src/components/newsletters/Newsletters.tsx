a99742d72e76ec3c9f0448b199c6dd258cee200e
Pavel <pavelmalyv@gmail.com>
2025-02-18 09:36:11 +0300

    feat(Newsletters): добавить react-hook-form

--- src/components/newsletters/Newsletters.tsx
+++ src/components/newsletters/Newsletters.tsx
@@ -3,10 +3,26 @@ import Field from '../UI/field/Field';
 import Section from '../UI/section/Section';
 import Button from '../UI/button/Button';
 
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Controller, useForm } from 'react-hook-form';
+import { object, string } from 'yup';
 import { useId } from 'react';
+
+const formSchema = object({
+	email: string().email().required(),
+});
 
 const Newsletters = () => {
 	const idHead = useId();
+
+	const { control, handleSubmit } = useForm({
+		resolver: yupResolver(formSchema),
+		defaultValues: {
+			email: '',
+		},
+	});
+
+	const onSubmit = () => {};
 
 	return (
 		<Section
@@ -19,19 +35,38 @@ const Newsletters = () => {
 			center={true}
 			marginBottom="large"
 		>
-			<form className={cl.form} aria-labelledby={idHead} noValidate={true}>
+			<form
+				className={cl.form}
+				onSubmit={handleSubmit(onSubmit)}
+				aria-labelledby={idHead}
+				noValidate={true}
+			>
 				<h3 id={idHead} className={'visually-hidden'}>
 					Subscribe to the newsletter
 				</h3>
 
 				<div className={cl.body}>
-					<Field
-						type="email"
-						label="Your email"
-						isLabelHidden={true}
-						placeholder="Enter your email"
-						autoComplete="email"
-						aria-required={true}
+					<Controller
+						name="email"
+						control={control}
+						render={({ field, fieldState }) => (
+							<Field
+								type="email"
+								label="Your email"
+								isLabelHidden={true}
+								placeholder="Enter your email"
+								autoComplete="email"
+								onChange={field.onChange}
+								onBlur={field.onBlur}
+								value={field.value}
+								disabled={field.disabled}
+								name={field.name}
+								ref={field.ref}
+								aria-required={true}
+								aria-invalid={fieldState.invalid}
+								errorMessage={fieldState.error?.message}
+							/>
+						)}
 					/>
 
 					<Button type="submit" mobileFull={true}>
