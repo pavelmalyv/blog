6e07a0cb148d1fe86f33e5e0ad81a2c744a93b4b
Pavel <pavelmalyv@gmail.com>
2025-02-18 11:13:44 +0300

    feat(Button): добавить disabled

--- src/components/UI/button/Button.tsx
+++ src/components/UI/button/Button.tsx
@@ -4,13 +4,15 @@ import cl from './Button.module.scss';
 interface ButtonProps {
 	type?: 'button' | 'submit' | 'reset';
 	mobileFull?: boolean;
+	disabled?: boolean;
 	children: React.ReactNode;
 }
 
-const Button = ({ type = 'button', mobileFull = false, children }: ButtonProps) => {
+const Button = ({ type = 'button', mobileFull = false, disabled, children }: ButtonProps) => {
 	return (
 		<button
 			type={type}
+			disabled={disabled}
 			className={classNames(cl.button, { [cl['button_mobile-full']]: mobileFull })}
 		>
 			{children}
