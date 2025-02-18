0c4e4d7dfaae28e7ed0d7b98b7844b3cff5218da
Pavel <pavelmalyv@gmail.com>
2025-02-18 17:49:36 +0300

    feat(PostsList): добавить вертикальный вариант

--- src/components/postsList/PostsList.tsx
+++ src/components/postsList/PostsList.tsx
@@ -1,15 +1,17 @@
 import cl from './PostsList.module.scss';
+import classNames from 'classnames';
 import PostCard from '../postCard/PostCard';
 
 import { Posts } from '../../types/posts';
 
 interface PostsListProps {
 	posts: Posts | null[];
+	direction?: 'horizontal' | 'vertical';
 }
 
-const PostsList = ({ posts }: PostsListProps) => {
+const PostsList = ({ posts, direction = 'horizontal' }: PostsListProps) => {
 	return (
-		<ul className={cl.list}>
+		<ul className={classNames(cl.list, cl[`list_${direction}`])}>
 			{posts.map((post, i) => {
 				const key = post ? post.id : i;
 
