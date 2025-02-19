5407ce02ad7e705204cd4df9fdcf87fd2eec72bb
Pavel <pavelmalyv@gmail.com>
2025-02-19 14:26:13 +0300

    feat(Section): добавить аттрибут aria-labelledby

--- src/components/UI/section/Section.tsx
+++ src/components/UI/section/Section.tsx
@@ -7,13 +7,14 @@ interface SectionProps {
 	subtitle?: string;
 	description?: string;
 	children: React.ReactNode;
-	labelledby?: boolean;
+	isLabelledbyTitle?: boolean;
 	container?: boolean;
 	width?: 'medium' | 'full';
 	titleSize?: 'h1' | 'h2';
 	titleLevel?: 1 | 2;
 	center?: boolean;
 	marginBottom?: 'medium' | 'large';
+	'aria-labelledby'?: string;
 }
 
 const Section = ({
@@ -21,19 +22,26 @@ const Section = ({
 	subtitle,
 	description,
 	children,
-	labelledby = true,
+	isLabelledbyTitle = true,
 	container = true,
 	width = 'full',
 	titleSize = 'h2',
 	titleLevel = 2,
 	center = false,
 	marginBottom = 'medium',
+	['aria-labelledby']: ariaLabelledbyProp,
 }: SectionProps) => {
 	const idTitle = useId();
-	const ariaLabelledby = labelledby && title ? idTitle : undefined;
 	const classesHead = classNames(cl.head, cl[`head_${width}`], cl[`head_${marginBottom}`], {
 		[cl['head_center']]: center,
 	});
+
+	let ariaLabelledby;
+	if (ariaLabelledbyProp) {
+		ariaLabelledby = ariaLabelledbyProp;
+	} else if (isLabelledbyTitle && title) {
+		ariaLabelledby = idTitle;
+	}
 
 	return (
 		<section className="section" aria-labelledby={ariaLabelledby}>
