dd59c368d28333484fa1473a8e78591755c51603
Pavel <pavelmalyv@gmail.com>
2025-02-19 13:55:38 +0300

    feat(AuthorPage): добавить вывод ошибки

--- src/pages/AuthorPage.tsx
+++ src/pages/AuthorPage.tsx
@@ -1,20 +1,32 @@
 import Heading from '../components/UI/heading/Heading';
+import ErrorMessage from '../components/UI/errorMessage/ErrorMessage';
 
 import { useParams } from 'react-router';
-import { throwNotFoundIfInvalid } from '../utils/error';
 import { useGetUserByIdQuery } from '../api/usersSlice';
+import { throwNotFoundIfInvalid, throwNotFoundIfStatus } from '../utils/error';
+import { ERROR_MESSAGES } from '../constants/error';
 
 const AuthorPage = () => {
 	const params = useParams<{ id?: string }>();
 	const authorId = throwNotFoundIfInvalid(params.id);
-	const { data: user } = useGetUserByIdQuery(authorId);
+	const { data: user, isError, error } = useGetUserByIdQuery(authorId);
 
-	const fullName = user ? `${user.firstName} ${user.lastName}` : null;
-	return (
-		<>
-			<Heading title={fullName} />
-		</>
-	);
+	let body: React.ReactNode;
+	if (isError) {
+		throwNotFoundIfStatus(error);
+
+		body = <ErrorMessage message={ERROR_MESSAGES.authorLoad} />;
+	} else {
+		const fullName = user ? `${user.firstName} ${user.lastName}` : null;
+
+		body = (
+			<>
+				<Heading title={fullName} />
+			</>
+		);
+	}
+
+	return body;
 };
 
 export default AuthorPage;
