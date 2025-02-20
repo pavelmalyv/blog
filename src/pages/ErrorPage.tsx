439a11b50e63705e624b1759b15984c03ec19d55
Pavel <pavelmalyv@gmail.com>
2025-02-20 17:39:11 +0300

    feat(ErrorPage): добавить содержимое страницы ошибки

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,20 +1,36 @@
 import Root from './Root';
 import Error from '../components/error/Error';
 
-import { useRouteError } from 'react-router';
+import { isRouteErrorResponse, useRouteError } from 'react-router';
 
 const ErrorPage = () => {
 	const error = useRouteError();
-	console.log(error);
+
+	let title: string;
+	let description: string;
+	let isLink: boolean = false;
+
+	if (isRouteErrorResponse(error)) {
+		title = String(error.status);
+
+		switch (error.status) {
+			case 404:
+				description =
+					'The page you are looking for does not exist or has been moved. Check the URL or return to the homepage';
+				isLink = true;
+				break;
+			default:
+				description = 'Please try again later';
+		}
+	} else {
+		title = 'Error';
+		description = 'A critical error has occurred. Try refreshing the page';
+	}
 
 	return (
 		<Root>
-			<Error
-				title="404"
-				description="The page you are looking for does not exist or has been moved. Check the URL or return to the homepage"
-			/>
+			<Error title={title} description={description} isLink={isLink} />
 		</Root>
 	);
 };
-
 export default ErrorPage;
