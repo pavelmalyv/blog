85f3d6fedf9c3046abe0a544564b3b0c5942d861
Pavel <pavelmalyv@gmail.com>
2025-02-20 16:52:45 +0300

    feat(Button): убрать пропс mobileFull

--- src/components/UI/button/Button.tsx
+++ src/components/UI/button/Button.tsx
@@ -1,28 +1,15 @@
-import classNames from 'classnames';
 import cl from './Button.module.scss';
 
 interface ButtonProps {
 	type?: 'button' | 'submit' | 'reset';
-	mobileFull?: boolean;
 	disabled?: boolean;
 	children: React.ReactNode;
 	onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-const Button = ({
-	type = 'button',
-	mobileFull = false,
-	disabled,
-	children,
-	onClick,
-}: ButtonProps) => {
+const Button = ({ type = 'button', disabled, children, onClick }: ButtonProps) => {
 	return (
-		<button
-			type={type}
-			disabled={disabled}
-			className={classNames(cl.button, { [cl['button_mobile-full']]: mobileFull })}
-			onClick={onClick}
-		>
+		<button type={type} disabled={disabled} className={cl.button} onClick={onClick}>
 			{children}
 		</button>
 	);
