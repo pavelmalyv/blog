0d09453326da42934b5aa5d314eff2170ff91801
Pavel <pavelmalyv@gmail.com>
2025-04-15 12:34:50 +0300

    refactor(FormSmall): уменьшить дублирование

--- src/components/Forms/formSmall/FormSmall.tsx
+++ src/components/Forms/formSmall/FormSmall.tsx
@@ -1,30 +1,13 @@
 import classNames from 'classnames';
 import cl from './FormSmall.module.scss';
 
-import { useId } from 'react';
-
-interface FormSmallProps {
-	title: string;
-	isHiddenTitle?: boolean;
+type FormSmallProps = React.ComponentProps<'form'> & {
 	children: React.ReactNode;
-	noValidate?: boolean;
-	onSubmit: React.FormEventHandler<HTMLFormElement>;
-}
+};
 
-const FormSmall = ({
-	title,
-	isHiddenTitle = true,
-	children,
-	noValidate,
-	onSubmit,
-}: FormSmallProps) => {
-	const headId = useId();
-
+const FormSmall = ({ children, className, ...props }: FormSmallProps) => {
 	return (
-		<form className={cl.form} aria-labelledby={headId} onSubmit={onSubmit} noValidate={noValidate}>
-			<h3 id={headId} className={classNames({ 'visually-hidden': isHiddenTitle })}>
-				{title}
-			</h3>
+		<form className={classNames(cl.form, className)} {...props}>
 			{children}
 		</form>
 	);
