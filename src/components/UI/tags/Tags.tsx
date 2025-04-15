fc1cc8a241170d0986e13dab169b040595ef0d6d
Pavel <pavelmalyv@gmail.com>
2025-04-15 11:17:11 +0300

    refactor(Tags): улучшить читаемость

--- src/components/UI/tags/Tags.tsx
+++ src/components/UI/tags/Tags.tsx
@@ -1,7 +1,6 @@
 import type { Tags } from '@/types/tags';
 
 import cl from './Tags.module.scss';
-import classNames from 'classnames';
 import Skeleton from 'react-loading-skeleton';
 
 interface TagsProps {
@@ -11,28 +10,17 @@ interface TagsProps {
 const Tags = ({ tags }: TagsProps) => {
 	const tagsList = tags ?? (new Array(3).fill(null) as null[]);
 
-	const style = {
-		height: 24,
-		borderRadius: 16,
-	};
-
 	return (
 		<ul className={cl.tags}>
-			{tagsList.map((item, i) => {
-				if (item) {
-					return (
-						<li className={classNames(cl.item, cl['item-tag'])} key={item} style={style}>
-							{item}
-						</li>
-					);
-				} else {
-					return (
-						<li className={cl.item} key={i}>
-							<Skeleton width="4em" height={style.height} borderRadius={style.borderRadius} />
-						</li>
-					);
-				}
-			})}
+			{tagsList.map((item, i) => (
+				<li key={item ?? i} className={cl.item}>
+					{item ? (
+						<div className={cl.tag}>{item}</div>
+					) : (
+						<Skeleton className={cl.skeleton} width="4em" />
+					)}
+				</li>
+			))}
 		</ul>
 	);
 };
